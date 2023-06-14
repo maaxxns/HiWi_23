d8f3f2fca06d995e714740d32d89ce4f70e67e64
maaxxns <max.koch@tu-dortmund.de>
2023-06-14 15:08:14 +0200

    I added a new phase unwrapping process which seems to work nicely. However, I still dont get the desired refrective index of SI.
    I also added some comments for readibility

--- programm_thz_analysis/THz.py
+++ programm_thz_analysis/THz.py
@@ -2,19 +2,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 import uncertainties as un
-from matplotlib.cm import get_cmap
 from scipy.fft import fft, fftfreq
 from scipy.signal import find_peaks
 from scipy.constants import c
-
+from scipy.optimize import curve_fit
 ###################################################################################################################################
 # Functions
+def lin(A, B, x):
+    return A*x+B
 
-def H_0(data_ref, data_sam): #takes in two spectral amplitudes and dives them to return transfer function
+def H_0(data_ref, data_sam): #takes in two spectral amplitudes and dives them to return complex transfer function
     return (data_sam/data_ref)
 
 def n(freq, d, phase): # takes in the frequency of the dataset, the thickness of the sample d and the frequency resolved phase and returns the real refractive index
-    return (1 - c/(freq* d) *phase)
+    return (1 - c/(freq* d) *phase) #    return (1 - c/(freq* d) *phase)
+
 
 def k(freq, d, H_0, n): # takes in the frequency of the dataset, the thickness of the sample d and the frequency resolved H_0 and returns the complex refractive index
     n_real = n
@@ -31,11 +33,40 @@ def FFT_func(I, t): # FFT
     FDelay = fftfreq(N, d=timestep)[:N//2] #FFT of the time to frequencies. 
     return [FDelay[10:], FX[10:]] # cut of the noise frequency
 
+def unwrapping_alt(amp_ref, amp_sam, freq_ref): #unwrapping from paper Phase_correction_in_THz_TDS_JIMT_revision_clean.pdf
+    angle_ref = np.angle(amp_ref)
+    angle_sam = np.angle(amp_sam)
+    phase_dif = (np.unwrap(abs(angle_ref - angle_sam)))
+    params,_ = curve_fit(lin, freq_ref, phase_dif)
+    phase_dif_0 = phase_dif - 2*np.pi*np.floor(params[1]/np.pi)
+    phase_ref_0 = freq_ref * t_peak_ref
+    phase_sam_0 = freq_sam * t_peak_sam
+    phase_offset = freq_ref * (t_peak_sam - t_peak_ref)
+    return phase_dif_0 - phase_ref_0 + phase_sam_0 + phase_offset
+
+
+
+###################################################################################################################################
+#       All data is read in, in this block.
+#       Necessary data are
+#       - the time resolved THz measurment
+#       - the thickness of the probe
 ###################################################################################################################################
 
+
+# The thickness of the probe
+
+d = 0.52*10**(-3) # thickness of the probe in SI
+
 #Read the excel file
-data_sam = np.genfromtxt('data/without_cryo_with_purge_teflon.txt', delimiter="	", comments="#")
-data_ref = np.genfromtxt('data/without_cryo_with_purge_teflon_2.txt',  delimiter="	", comments="#")
+
+data_sam = np.genfromtxt('data/SI_measurement/SI_purge.txt', delimiter="	", comments="#") # The time resolved dataset of the probe measurment
+data_ref = np.genfromtxt('data/SI_measurement/SI_reference.txt',  delimiter="	", comments="#") # the time resolved dataset of the reference measurment
+
+
+###################################################################################################################################
+# This block plots the raw dataset
+###################################################################################################################################
 
 
 data_ref[:,0] = data_ref[:,0] * 10**(-12) # ps in seconds
@@ -43,15 +74,6 @@ data_sam[:,0] = data_sam[:,0] * 10**(-12)
 
 data_ref[:,0] = data_ref[:,0] + np.abs(np.min(data_ref[:,0])) # move everything to positiv times
 data_sam[:,0] = data_sam[:,0] + np.abs(np.min(data_sam[:,0]))
-
-peaks_inref,_ = find_peaks(data_ref[:,1], prominence = 0.3) # height=np.max(data_ref[:,1])
-peaks_insam,_ = find_peaks(data_sam[:,1], prominence = 0.3)
-
-cap_index_ref = abs(peaks_inref[0] - peaks_inref[-1])//2 + peaks_inref[0]
-cap_index_sam = abs(peaks_insam[0] - peaks_insam[-1])//2 + peaks_insam[0]
-
-#data_ref = data_ref[:cap_index_ref]
-#data_sam = data_sam[:cap_index_sam]
 
 #plot the intensity against time delay
 plt.figure()
@@ -63,10 +85,30 @@ plt.ylabel('Amplitude')
 plt.legend()
 plt.grid()
 plt.title('The reference and sample data set')
-plt.savefig('build/THz1.pdf')
+plt.savefig('build/THz_timedomain.pdf')
 plt.close()
 
-#########################################################################################
+##################################################################################################################################
+# Some necessary calculations on frequency and time resolution
+##################################################################################################################################
+
+N = len(data_ref[:,0]) #number of total data points
+timestep = np.abs(data_ref[:,0][2]-data_ref[:,0][3]) # minimum time resolution
+Delta_f = 1/(N*timestep) #frequency resolution
+print('Delta t = ', " ",timestep/10**(-12), "ps")
+print("T ", N*timestep/10**(-12), "ps")
+print("Delta f = ", " ", Delta_f*10**(-12), "THz")
+
+peak_ref,prop = find_peaks(data_ref[:,1], prominence=1) # finds the highest peak in the dataset and returns its index
+peak_ref = peak_ref[0]
+peak_sam,prop = find_peaks(data_sam[:,1], prominence=1)
+peak_sam = peak_sam[0]
+
+t_peak_ref = data_ref[peak_ref,0] # the time value of the highest peak in the dataset
+t_peak_sam = data_sam[peak_sam,0]
+###################################################################################################################################
+# This block applies the FFT to the data, aswell as masking frequencies that we dont need for the analization
+###################################################################################################################################
 
 freq_ref, amp_ref = FFT_func(data_ref[:,1], data_ref[:,0])  #in Hz
 freq_sam, amp_sam = FFT_func(data_sam[:,1], data_sam[:,0])
@@ -78,10 +120,7 @@ freq_ref = freq_ref[mask1]
 freq_sam = freq_sam[mask1]
 
 ##########################################################################################
-# read in data that is used for refference
-
-material_properties_ref = np.genfromtxt('data/teflon_1/teflon_1_material_properties.txt', delimiter="	", comments="#")[10:]
-
+# This block plots the FFT
 ##########################################################################################
 
 plt.figure()
@@ -92,12 +131,14 @@ plt.ylabel('Spectral Amplitude')
 plt.legend()
 plt.grid()
 plt.title('The FFT of the data sets')
-plt.savefig('build/THz4_1.pdf')
+plt.savefig('build/THz_FFT.pdf')
 plt.close()
 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-freq_ref = freq_ref # we need to have same shapes
+###################################################################################################################################
+# This block calculates the complex transfer function and does the unwrapping porcess
+# It also plots the angle and phase of the transferfunction
+###################################################################################################################################
 
 H_0_value = H_0(amp_ref, amp_sam) # complex transfer function
 
@@ -105,46 +146,54 @@ angle = np.angle(H_0_value) #angle between complex numbers
 phase = (np.unwrap(angle))  #phase 
 
 
+phase_dif = unwrapping_alt(amp_ref, amp_sam, freq_ref)
+
+# Note that phase and phase_dif should be equal. However, there are not. So either the paper is wrong. Or I am doing something wrong
+
 plt.figure()
-plt.plot(freq_ref*10**(-12),angle, label='angle')
-plt.plot(freq_ref*10**(-12),phase, label='Phase')
+plt.plot(freq_ref*10**(-12),phase_dif, label='phase differenz')
+plt.plot(freq_ref*10**(-12),phase, label='phase directly from H_0')
 #plt.plot(data_ref[:,0], filter_ref)
 plt.xlabel(r'$ \omega/THz $')
 plt.ylabel(r'$\Phi$')
 plt.legend()
 plt.grid()
-plt.title('The phase and the complex angle. Note the angle is unwrapped.')
+plt.title('The phase unwarpped')
 plt.savefig('build/THzPhase.pdf')
 plt.close()
 
+###################################################################################################################################
+#   This block calculates the real and complex part of the refractive index
+###################################################################################################################################
 
-d = 0.26*10**(-3) #millimeter
 n_real = n(freq_ref, d, phase)
+n_real_alt = n(freq_ref, d, phase_dif)
 n_im = k(freq_ref, d, H_0_value, n_real)
 
+###################################################################################################################################
+#   This block plots the real part of the refractive index
+###################################################################################################################################
 
 plt.figure()
-#plt.plot(freq_ref*10**(-12),phase, label='phase')
+plt.plot(freq_ref*10**(-12), n(freq_ref, d, phase_dif), label='real part of refractive index calculated by the alternative unwrap')
 plt.plot(freq_ref*10**(-12), n_real, label='real part of refractive index')
-plt.plot(material_properties_ref[10:,0], material_properties_ref[10:,1], label='refference n from steffens program')
-#plt.plot(material_properties_ref[10:,0], material_properties_ref[10:,2], label='refference k from steffens program')
-#plt.plot(data_ref[:,0], filter_ref)
 plt.xlabel(r'$ \omega/THz $')
 plt.ylabel('n (arb.)')
 plt.xlim(0.18, 4.5)
-plt.ylim(0,2)
+#plt.ylim(0,4)
 plt.legend()
 plt.grid()
 plt.title('The real part of the refractive index')
-plt.savefig('build/THz5_1.pdf')
+plt.savefig('build/THz_real_index.pdf')
 plt.close()
 
-
 ##################################################################################################
+#   This block plots the complex part of the refractive index
+###################################################################################################################################
 
 plt.figure()
+plt.plot(freq_ref*10**(-12), k(freq_ref, d, H_0_value, n_real_alt), label='complex part of refractive index by alt')
 plt.plot(freq_ref*10**(-12), n_im, label='complex part of refractive index')
-#plt.plot(material_properties_ref[10:,0], material_properties_ref[10:,2], label='refference n from steffens program')
 plt.xlabel(r'$ \omega/THz $')
 plt.ylabel('k (arb.)')
 plt.xlim(0.18, 4.5)
@@ -152,14 +201,18 @@ plt.xlim(0.18, 4.5)
 plt.legend()
 plt.grid()
 plt.title('The complex part of the refractive index')
-plt.savefig('build/THz5_2.pdf')
+plt.savefig('build/THz_complex_index.pdf')
 plt.close()
+
+###################################################################################################################################
+# This block calculates the absorption coefficient and plots it
+###################################################################################################################################
 
 alpha = 2*freq_ref *n_im/c 
 
 plt.figure()
 plt.plot(freq_ref*10**(-12), alpha/100, label='Absorption coefficient')
-plt.plot(material_properties_ref[10:,0], material_properties_ref[10:,2], label='refference n from steffens program')
+#plt.plot(material_properties_ref[10:,0], material_properties_ref[10:,2], label='refference n from steffens program')
 #plt.plot(data_ref[:,0], filter_ref)
 plt.xlabel(r'$ \omega/THz $')
 plt.ylabel(r'$\alpha$')
@@ -168,5 +221,5 @@ plt.ylim(0, 500)
 plt.legend()
 plt.grid()
 plt.title('The absorption coeffiecient')
-plt.savefig('build/THz6.pdf')
+plt.savefig('build/THz_absorption.pdf')
 plt.close()
