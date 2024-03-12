8bfa0c80830bee87ff3dd70328d32b736a7bd1d1
maaxxns <max.koch@udo.edu>
2024-03-12 11:15:38 +0100

    FFT_and_stuff creates a FFT This process is repeated for every file inside the target directory, which makes plotting faster

---
+++ programm_thz_analysis/src/FFT_and_stuff.py
@@ -0,0 +1,38 @@
+from mathTHz import *
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+#######################################################################################################
+
+directory = "/20240301" # desired directory to make plots from
+
+#######################################################################################################
+#######################################################################################################
+
+directory = os.getcwd() + "/data" + directory #how to get all files names in a directory?
+datas = []
+i = 0
+
+with os.scandir(directory) as it: # scan directory
+    for entry in it:
+        if entry.name.endswith(".txt"): # search for every file that ends with .txt
+            datas.append([entry.name ,np.genfromtxt(entry.path, comments="#", delimiter="	", skip_header=2)]) # append those files to a big list which includes the data an name of file
+            i = i + 1
+
+print("A total of: ",i, " files were red")
+
+fft_datas = [None] * len(datas)
+i = 0
+for data in datas:
+    fft_datas[i] = [data[0], FFT_func(data[1][:,4], data[1][:, 2])] # make FFT of data
+    i = i + 1
+
+for fft_data in fft_datas: # plot FFT
+    plt.figure()
+    plt.xlim(0, 2)
+    plt.plot(fft_data[1][0], np.abs(fft_data[1][1]), label='FFT of ' + fft_data[0]) #not sure if the absolut of the FFT here is correct
+    plt.ylabel('Intensity')
+    plt.xlabel(r'$f / THz$')
+    plt.legend()
+    plt.savefig('build/FFTs/FFT_of_' + fft_data[0] + '.pdf')
