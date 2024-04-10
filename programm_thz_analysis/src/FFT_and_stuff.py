628a3aa6f280ca8ea5087f54c76a4cfe8a033799
Max_Koch <max.koch@tu-dortmund.de>
2024-04-10 17:53:01 +0200

    Added a test for unevenly distanced datapoints in case that ever happens. If the datapoints are unevenly spaced the progamm will do a cubic 1d interpolation of the datapoints and use that for the new FFT

--- programm_thz_analysis/src/FFT_and_stuff.py
+++ programm_thz_analysis/src/FFT_and_stuff.py
@@ -2,7 +2,7 @@ from mathTHz import *
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-
+from scipy.interpolate import CubicSpline
 #######################################################################################################
 
 directory = "/20240301" # desired directory to make plots from
@@ -24,8 +24,21 @@ print("A total of: ",i, " files were red")
 
 fft_datas = [None] * len(datas)
 i = 0
+unevenspacing = False
 for data in datas:
-    fft_datas[i] = [data[0], FFT_func(data[1][:,4], data[1][:, 2])] # make FFT of data
+    # check if data is actually evenly spaced otherwise we have to do a interpolation
+    t = np.abs(data[1][1, 4] - data[1][0, 4]) 
+    for i in np.range(len(data[:, 4]) - 1):
+        spacing = data[i + 1, 4] - data[i, 4]
+        if(np.abs(spacing - t) > 10**-6):
+            unevenspacing = True
+            print("Datapoints seem to be unevenly spaced, interpolation will be initiated")
+    if(unevenspacing):
+        spl = CubicSpline(data[1][:,4], data[1][:, 2])
+        xnew = np.linspace(data[1][0, 4], data[1][-1, 4], len(data[1][:,4])*5) # lets use five times the amount of "old" datapoints for the spline
+        fft_datas[i] = [data[0], xnew, spl(xnew)] 
+    else:
+        fft_datas[i] = [data[0], FFT_func(data[1][:,4], data[1][:, 2])] # make FFT of data
     i = i + 1
 
 for fft_data in fft_datas: # plot FFT
